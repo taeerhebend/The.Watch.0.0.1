294d959e8519387435b2bf0d4f333c5829e87100
Barton Mallory <tai_erhebend@outlook.com>
2024-01-24 17:42:59 -0600

    2.1

---
+++ Services/IErrorHandlingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWatch.Services
+{
+    public interface IErrorHandlingService
+    {
+        Task<string> HandleExceptionAsync(Exception ex);
+        Task<string> GenerateErrorReportAsync(Exception ex);
+        string GetExceptionDetails(Exception ex);
+        string GetCustomErrorMessage(string errorKey);
+        void LogException(Exception ex);
+    }
+
+    public class ErrorHandlingService : IErrorHandlingService
+    {
+    }
+}
