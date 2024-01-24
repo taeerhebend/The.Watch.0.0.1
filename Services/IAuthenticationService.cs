290f70cbf6f12fdcd0e1e922fe92d9f9630d90b6
Barton Mallory <tai_erhebend@outlook.com>
2024-01-24 16:00:07 -0600

    X1

--- Services/IAuthenticationService.cs
+++ Services/IAuthenticationService.cs
@@ -19,5 +19,34 @@ namespace TheWatch.Services
 
     public class AuthenticationService : IAuthenticationService
     {
+        public Task<bool> ChangePasswordAsync(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ForgotPasswordAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> RegisterAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ResetPasswordAsync(string username, string code, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> SignInAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignOutAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
